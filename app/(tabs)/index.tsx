64615c0217f1ee44f3669457f96866b94a8bb82d
devsujalpatel <sujal7455@gmail.com>
2025-11-20 04:08:23 +0530

    Header component created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,60 +1,26 @@
-import { api } from "@/convex/_generated/api";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
 import { useTheme } from "@/hooks/useTheme";
-import { useMutation, useQuery } from "convex/react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
-
-  const todo = useQuery(api.todos.getTodos);
-  console.log(todo);
-
-  const addTodo = useMutation(api.todos.addTodo);
-  const clearAllTodos = useMutation(api.todos.clearAllTodos);
+  const { toggleDarkMode, colors } = useTheme();
+  const homeStyles = createHomeStyles(colors);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hi, This is home page</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>toggle the mode</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => addTodo({ text: "create a app" })}>
-        <Text>Add a new todo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => clearAllTodos()}>
-        <Text>Clear all</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>toggle the mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 20,
-    textAlign: "center",
-    padding: 10,
-    cursor: "pointer",
-  },
-  link: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-    marginBottom: 20,
-    textAlign: "center",
-    backgroundColor: "#0c524fff",
-    padding: 10,
-    cursor: "pointer",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
